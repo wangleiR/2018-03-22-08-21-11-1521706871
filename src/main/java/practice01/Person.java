44058cc06d37b8ea694db069825d8068c6dc068f
wangleiR <1220632559@qq.com>
2018-04-08 00:31:46 +0800

    Practic 01

--- src/main/java/practice01/Person.java
+++ src/main/java/practice01/Person.java
@@ -1,4 +1,33 @@
 package practice01;
 
 public class Person {
+    private String name;
+    private int age;
+
+
+    public Person(String name,int age) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public int getAge() {
+
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String introduce() {
+        return  "My name is Tom. I am 21 years old.";
+    }
 }
